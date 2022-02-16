9ddd639372a40e32b4fafd2981750188832279e4
Rahima Salmanova <rehimesalman7@gmail.com>
2022-02-16 14:04:34 +0400

    Addin first image to cloudinary

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -5,15 +5,16 @@ const Campground = require('../models/campground');
 const catchAsync = require('../utils/catchAsync');
 const {isLoggedIn, isAuthor, validateCampground } = require('../middleware');
 const multer  = require('multer');
-const upload = multer({ dest: 'uploads/' });
+const {storage} = require('../cloudinary')
+const upload = multer({ storage });
 
 //Grouping same routes into one
 router.route('/')
     .get(catchAsync(campgrounds.index))
     // .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
     .post(upload.array('image'), (req, res) => {
-       // console.log(req.body, req.files);
-       // res.send('it worked')
+       console.log(req.body, req.files);
+       res.send('it worked')
     })
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
