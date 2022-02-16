514fa570cfebcaf043a3d455f47013e3182f473f
Rahima Salmanova <rehimesalman7@gmail.com>
2022-02-16 10:57:44 +0400

    Dotenv environment variables added

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -12,8 +12,8 @@ router.route('/')
     .get(catchAsync(campgrounds.index))
     // .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
     .post(upload.array('image'), (req, res) => {
-       console.log(req.body, req.files);
-       res.send('it worked')
+       // console.log(req.body, req.files);
+       // res.send('it worked')
     })
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
