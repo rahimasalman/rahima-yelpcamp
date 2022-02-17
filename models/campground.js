d08dbf0eaf1a81c0c04c3bebc80b5edb6d4c5259
Rahima Salmanova <rehimesalman7@gmail.com>
2022-02-17 15:30:31 +0400

    Storing images links in mongo is done

--- models/campground.js
+++ models/campground.js
@@ -4,7 +4,12 @@ const Schema = mongoose.Schema;
 const CampgroundSchema = new Schema({
     title: String,
     price: Number,
-    image: String,
+    images: [
+        {
+            url: String,
+            filename: String
+        }
+    ],
     description: String,
     location: String,
     author: {
