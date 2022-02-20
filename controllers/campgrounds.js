976dc7f6a30a79557ba3eec8e23d14c589072356
Rahima Salmanova <rehimesalman7@gmail.com>
2022-02-20 13:04:39 +0400

    Add cloudinary to the campground controller

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,5 +1,6 @@
 const Campground = require('../models/campground');
 const {isLoggedIn, isAuthor, validateCampground } = require('../middleware');
+const { cloudinary } = require("../cloudinary");
 
 module.exports.index = async (req, res) => {
     const campgrounds = await Campground.find({});
@@ -46,11 +47,18 @@ module.exports.renderEditForm = async (req, res) => {
 };
 module.exports.updateCampground = async (req, res) => {
     const { id } = req.params;
-    console.log(req.body)
+    console.log(req.body);
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     const imgs = req.files.map(f => ({url: f.path, filename: f.filename}));
     campground.images.push(...imgs);
     await campground.save();
+    if (req.body.deleteImages) {
+        for (let filename of req.body.deleteImages) {
+           await cloudinary.uploader.destroy(filename);
+        }
+        await campground.updateOne({ $pull: { images: { filename: { $in: req.body.deleteImages } } } });
+        console.log(campground);
+    };
     req.flash('success', 'Campground updated successfully!!!');
     res.redirect(`/campgrounds/${campground._id}`);
 };
