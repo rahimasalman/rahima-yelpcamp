4e1241653d7fff29c0887efcfcc67fc1893f8495
Rahima Salmanova <rehimesalman7@gmail.com>
2022-02-21 16:57:10 +0400

    Added default marker to the web map for campground location

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -2,6 +2,10 @@ mapboxgl.accessToken = mapToken;
     const map = new mapboxgl.Map({
         container: 'map', // container ID
         style: 'mapbox://styles/mapbox/streets-v11', // style URL
-        center: [-74.5, 40], // starting position [lng, lat]
-        zoom: 5 // starting zoom
+        center:  campground.geometry.coordinates, // starting position [lng, lat]
+        zoom: 10 // starting zoom
     });
+
+ new mapboxgl.Marker({ color: '#858bee', })
+.setLngLat(campground.geometry.coordinates)
+.addTo(map);
