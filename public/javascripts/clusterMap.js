0edf169b6268f627d0b2b6eb09b6a3b8653c9ace
Rahima Salmanova <rehimesalman7@gmail.com>
2022-02-24 16:31:48 +0400

    Added basic clustering for campgrounds (partially done)

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -14,7 +14,7 @@ map.addSource('earthquakes', {
 type: 'geojson',
 // Point to GeoJSON data. This example visualizes all M1.0+ earthquakes
 // from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
-data: 'https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson',
+data: campgrounds,
 cluster: true,
 clusterMaxZoom: 14, // Max zoom to cluster points on
 clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
@@ -100,7 +100,8 @@ zoom: zoom
 // the unclustered-point layer, open a popup at
 // the location of the feature, with
 // description HTML from its properties.
-map.on('click', 'unclustered-point', (e) => {
+   map.on('click', 'unclustered-point', (e) => {
+
 const coordinates = e.features[0].geometry.coordinates.slice();
 const mag = e.features[0].properties.mag;
 const tsunami =
@@ -121,7 +122,7 @@ new mapboxgl.Popup()
 .addTo(map);
 });
  
-map.on('mouseenter', 'clusters', () => {
+   map.on('mouseenter', 'clusters', () => {
 map.getCanvas().style.cursor = 'pointer';
 });
 map.on('mouseleave', 'clusters', () => {
