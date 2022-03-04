c23f6e5e1b60f5e9b9f84b8380e4fc956f3e61e0
Rahima Salmanova <rehimesalman7@gmail.com>
2022-03-04 16:34:59 +0400

    Add content Security Policy for fonts, urls, images

--- app.js
+++ app.js
@@ -54,14 +54,62 @@ app.use(mongoSanitize({
      replaceWith: '_',
 }));
 
-// This disables the `contentSecurityPolicy` middleware but keeps the rest.
+// Content security policy
+app.use(helmet());
+
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://api.mapbox.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net/",
+    "https://res.cloudinary.com/rahima/"
+];
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com/",
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.mapbox.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+    "https://cdn.jsdelivr.net/",
+    "https://res.cloudinary.com/rahima/"
+];
+const connectSrcUrls = [
+    "https://*.tiles.mapbox.com",
+    "https://api.mapbox.com",
+    "https://events.mapbox.com",
+    "https://res.cloudinary.com/rahima/"
+];
+const fontSrcUrls = [ "https://res.cloudinary.com/rahima/" ];
+ 
 app.use(
-  helmet({
-    contentSecurityPolicy: false,
-  })
+    helmet.contentSecurityPolicy({
+        directives : {
+            defaultSrc : [],
+            connectSrc : [ "'self'", ...connectSrcUrls ],
+            scriptSrc  : [ "'unsafe-inline'", "'self'", ...scriptSrcUrls ],
+            styleSrc   : [ "'self'", "'unsafe-inline'", ...styleSrcUrls ],
+            workerSrc  : [ "'self'", "blob:" ],
+            objectSrc  : [],
+            imgSrc     : [
+                "'self'",
+                "blob:",
+                "data:",
+                "https://res.cloudinary.com/rahima/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT!
+                "https://images.unsplash.com/"
+            ],
+            fontSrc    : [ "'self'", ...fontSrcUrls ],
+            mediaSrc   : [ "https://res.cloudinary.com/rahima/" ],
+            childSrc   : [ "blob:" ]
+        }
+    })
 );
 
+
 const sessionConfig = {
+    name: 'session',
     secret: 'fake secret',
     resave: false,
     saveUninitialized: true,
